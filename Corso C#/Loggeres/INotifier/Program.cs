f5d127d71aa83ade4b6fe3adcbe1e02531191076
SalsarDev <capassodanielemichele@gmail.com>
2025-06-03 09:46:58 +0200

    Add files via upload

---
+++ Corso C#/Loggeres/INotifier/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public interface INotifier
+{
+    void Send(string message);
+}
+
+public class SmsNotifier : INotifier
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Messaggio inviato: {message}");
+    }
+}
+
+public class AlertService
+{
+    public void SendAlert(string message, INotifier notifier)
+    {
+        notifier.Send(message);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var alertService = new AlertService();
+        var smsNotifier = new SmsNotifier();
+
+        alertService.SendAlert("Allarme attivato!", smsNotifier);
+    }
+}
